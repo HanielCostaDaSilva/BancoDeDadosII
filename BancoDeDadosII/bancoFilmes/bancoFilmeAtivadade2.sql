2c5c54f905b41ed3e20de8922ec8032bb319f7cb
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2023-03-14 10:51:29 -0300

    Inserido a Atividade2 e aula 04

--- BancoDeDadosII/bancoFilmes/bancoFilmeAtivadade2.sql
+++ BancoDeDadosII/bancoFilmes/bancoFilmeAtivadade2.sql
@@ -37,7 +37,6 @@ ALTER TABLE Filme ADD CONSTRAINT FKFilme2Estud FOREIGN KEY(CodEst) REFERENCES Es
 ALTER TABLE Personagem ADD CONSTRAINT FKPersonagem2Artis FOREIGN KEY(CodART) REFERENCES Artista;
 ALTER TABLE Personagem ADD CONSTRAINT FKPersonagem1Filme FOREIGN KEY(CodFILME) REFERENCES Filme;
 
-
 /*Insertions*/
 
 /*Categoria*/
@@ -113,59 +112,97 @@ insert into personagem values(9 ,8, 'Smolder Bravestone', 20000);
 insert into personagem values(10 , 9, 'Lily Houghton', 15000);
 select * from Personagem;
 
-/*8. Verifique quais são os artistas cadastrados ordenados pelo nome.*/
-select * 
-from Artista
-order by NomeArt;
+/*Tarefa 2*/
 
-/*9. Selecione os artistas que têm o nome iniciando com a letra ‘B’.*/
+/*1. Faça um select geral nas cinco tabelas e observe seus relacionamentos.*/
 
-select * 
-from Artista
-where nomeArt like 'B%';
-
-/*10.  Verifique os filmes que foram lançados em 2019.*/
-
-select * 
-from Filme
-where ano=2019;
-
-/*11.  Atualize o cachê dos artistas em 15%.*/
-
-Update Personagem
-set cache= cache+ (cache*0.15);
+select * from Estudio;
+select * from Categoria;
+select * from Filme;
+select * from artista;
 select * from Personagem;
 
-/*12.  Atualize o país de 3 artistas à sua escolha.*/
+/*2. Em seguida, realize a seguinte inserção e consulta: */
+
+insert into filme values(default,'Elvis',2022,120,null,3);
+select * from filme;
+
+
+/*3. Verifique quais os títulos dos filmes que possuem duração maior que 120 min?*/
+
+SELECT titulo 
+from filme
+where duracao>120;
+
+/*4. Na tabela ARTISTA, quais artistas possuem cidade nula*/
+
+Select * 
+from Artista
+where cidade is null;
+
+
+/*4. atualize as cidades nulas encontradas para três artistas.*/
 
 Update Artista
-set pais= 'USA'
-where codArt=4;
-
-Update Artista
-set pais= null
-where codArt=1;
-
-Update Artista
-set pais= 'Brazil'
-where codArt=6;
+set cidade = 'Cajazeiras'
+where codArt in(
+    select codArt
+    from Artista
+    where cidade is null
+    Limit 3
+);
 
 select * from Artista;
 
-/*13.  Insira 2 artistas brasileiros.*/
 
-insert into artista values(default,'Luccas Neto', 'Rio de Janeiro','Brazil','8/02/1992');
-insert into artista values(default,'Larissa Manoela', 'Guarapuava','Brazil',' 28/12/2000');
-select * from artista;
+/*5. Qual a descrição da categoria do filme ‘Coringa’?*/
 
-/*14.  Delete 1 artista recentemente incluído que não seja brasileiro.*/
+select fil.codfilme, fil.titulo, cat.DescCATEG 
+from Filme fil inner join Categoria cat on cat.codcateg= fil.codcateg
+where fil.titulo='Coringa';
 
 
-Alter table personagem drop constraint FKPersonagem2Artis; 
-ALTER TABLE personagem ADD CONSTRAINT FKPersonagem2Artis FOREIGN KEY(CodART) REFERENCES Artista on delete cascade;
-/*!!!!   Não é recomendável mudar uma constraint para remover um único valor, ainda mais se for utilizando on delete cascade. Isso pode encadear uma série de remoções indesejadas, !!!!*/
+/*6. Mostre os títulos de filmes, com seus nomes de estúdios e nomes de suas categorias.*/
 
-Delete FROM artista
-where NomeArt like 'Owen Wilson';
-select * from artista;
-select * from personagem;
+select fil.titulo, est.nomeest, cat.DescCATEG
+from filme fil inner join Estudio est on fil.codEst = est.codEst  
+inner join Categoria cat on fil.CodCATEG= cat.CodCATEG;
+
+
+/*7. Qual o nome dos artistas que fizeram o filme ‘Encontro Explosivo’?*/
+
+select art.NomeArt, pers.NomePers, fil.titulo
+from filme fil  join personagem pers on pers.CodFILME = fil.CodFILME 
+ join artista art on pers.CODART= art.CODART
+where fil.titulo ='Encontro Explosivo';
+
+
+/*8. 	Selecione os artistas que têm o nome iniciando com a letra ‘B’ e participaram de filmes da categoria ‘Aventura’.    */
+
+select art.NomeArt, fil.titulo
+from artista art 
+ join personagem pers on art.CODART= pers.CODART
+ join filme fil  on pers.CodFILME = fil.CodFILME 
+ join categoria cat on fil.CodCateg= cat.CodCateg
+where cat.DescCateg ='Aventura' and art.NomeArt like 'B%';
+
+
+/*9. Apresente quantos filmes existem por categoria*/
+
+select cat.DescCateg, count(*) as Quantidade
+from categoria cat inner join filme fil on fil.codcateg = cat.codcateg
+group by cat.desccateg;
+
+/*10.  Explique o que o seguinte comando faz e mostra*/
+select a.nomeart, p.nomepers
+from artista a left outer join personagem p on a.codart = p.codart;
+
+/*Esta busca irá mostrar todos os nomes de artistas e seus respectivos personagens, incluindo os que não possuen personagens. */
+
+
+/*11.  O que a seguinte consulta retorna? Explique.*/
+Select f.titulo as Filme, c.desccateg as Categoria
+From filme f right join categoria c on f.codcateg = c.codcateg
+Where c.codcateg is null;
+
+/*Esta consulta não apressenta nenhum retorno. Pois não existe filme sem ligação com categoria.*/
